a3c58bdfe7330b3ce5942bf19eddd2e7302fbea5
Bsearom <98922865+Bsearom@users.noreply.github.com>
2022-07-18 21:45:57 -0300

    criamos pageadmin e tpl admin, e resolvi um Message: Template header not found!

--- vendor/hcodebr/php-classes/src/PageAdmin.php
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -1,16 +1,12 @@
-<?php 
-
+<?php
+ 
 namespace Hcode;
-
+ 
 class PageAdmin extends Page {
-
-	public function __construct($opts = array(), $tpl_dir = "/views/admin/")
-	{
-
-		parent::__construct($opts, $tpl_dir);
-
-	}
-
+    public function __construct($opts = array(), $tpl_dir = "views/admin/")
+    {
+        parent::__construct($opts, $tpl_dir);    
+    }
 }
-
+ 
 ?>
